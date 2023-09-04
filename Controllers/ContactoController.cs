806c500dc3cf8b02977d08c7940ca758e3e0c70d
jesus73514145 <jesus_soria@usmp.pe>
2023-09-04 14:15:10 -0500

    grabando el contacto en la base de datos desde el formulario

--- Controllers/ContactoController.cs
+++ Controllers/ContactoController.cs
@@ -6,26 +6,30 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using petclinic.Models;
+using petclinic.Data;
 
 namespace petclinic.Controllers{
     public class ContactoController : Controller {
         private readonly ILogger<ContactoController> _logger;
-
-        public ContactoController(ILogger<ContactoController> logger){
+        private readonly ApplicationDbContext _context;
+        public ContactoController(ILogger<ContactoController> logger,
+        ApplicationDbContext context){
             _logger = logger;
+            _context = context;
         }
 
-        public IActionResult Index(){return View();}
+        public IActionResult Index(){   return View();  }
 
         [HttpPost]
         public IActionResult Create(Contacto objContacto){
+            _context.Add(objContacto);
+            _context.SaveChanges();
+            
             ViewData["Message"] = "Estimado " + objContacto.Name + ", te estaremos contactando pronto";
             return View("Index");
         }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error(){
-            return View("Error!");
-        }
+        public IActionResult Error(){   return View("Error!");     }
     }
 }
